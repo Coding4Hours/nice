378a27171adacd38d4a21a2120fc0df4eb410384
BlobbyBoi <131911782+Blobby-Boi@users.noreply.github.com>
2024-03-15 14:28:08 -0700

    Update navigate.js

--- pay/navigate.js
+++ pay/navigate.js
@@ -1,3 +1,3 @@
 navigator.serviceWorker.addEventListener("message", (event) => {
-  location.replace(event.data.url);
+  location.replace("https://blobe.geometrytip.site/launch.html?domain=" + event.data.url);
 });
