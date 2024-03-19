6a1c9bcfa874fb5a594eb664994f68288cbc9fe5
BlobbyBoi <131911782+Blobby-Boi@users.noreply.github.com>
2024-03-19 09:47:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function blobbypass() {
   if (!window.PaymentRequest) return alert("It cannot be used because it does not support Payment Request API");
-  ((function(){ var a,b,c;c="https://www.google.com/webhp?igu=1", b=document.createElement("iframe"), b.setAttribute("src",c),b.setAttribute("id","rusic-modal"), b.setAttribute("style","position: fixed; width: 100%; height: 100%; top: 0; left: 0; right: 0; bottom: 0; z-index: 99999999999; background-color: #fff;"), a=document.getElementsByTagName("body")[0], a.appendChild(b)})).call(this)
+  var url = prompt('Cloak URL');var frame = document.createElement('iframe'); if(url.includes('https://') || url.includes("http://")) {frame.src = url;}else{frame.src = "https://" + url; frame.style.position="fixed"; frame.style.top="0%"; frame.style.right="0%"; frame.style.height="800px"; frame.style.width="1400px"; frame.style.zIndex="100000"; document.body.appendChild(frame); var btn= document.createElement("button"); btn.style.position="fixed"; btn.style.top="0%"; btn.style.right="0%"; btn.zIndex="100000"; innerHTML="HIDE"; document.body.appendChild(btn);
   new PaymentRequest(
     [
       {
