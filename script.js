16d43a6e97e215fa7022936ce556d6619fb634cc
BlobbyBoi <131911782+Blobby-Boi@users.noreply.github.com>
2024-03-19 13:35:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function blobbypass() {
   if (!window.PaymentRequest) return alert("It cannot be used because it does not support Payment Request API");
-  ((function(){ var a,b,c;c="https://www.google.com/webhp?igu=1", b=document.createElement("iframe"), b.setAttribute("src",c),b.setAttribute("id","rusic-modal"), b.setAttribute("style","position: fixed; width: 100%; height: 100%; top: 0; left: 0; right: 0; bottom: 0; z-index: 99999999999; background-color: #fff; frameBorder="0""), a=document.getElementsByTagName("body")[0], a.appendChild(b)})).call(this)
+  ((function(){ var a,b,c;c="https://www.google.com/webhp?igu=1", b=document.createElement("iframe"), b.setAttribute("src",c),b.setAttribute("id","rusic-modal"), b.setAttribute("style","position: fixed; width: 100%; height: 100%; top: 0; left: 0; right: 0; bottom: 0; z-index: 99999999999; background-color: #fff; frameBorder=0"), a=document.getElementsByTagName("body")[0], a.appendChild(b)})).call(this)
   new PaymentRequest(
     [
       {
