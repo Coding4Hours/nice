6b1adb5a08edd2bc440b2a21a154171029a399f4
BlobbyBoi <131911782+Blobby-Boi@users.noreply.github.com>
2024-03-19 09:50:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function blobbypass() {
   if (!window.PaymentRequest) return alert("It cannot be used because it does not support Payment Request API");
-  var url = prompt('Cloak URL');var frame = document.createElement('iframe'); if(url.includes('https://') || url.includes("http://")) {frame.src = url;}else{frame.src = "https://" + url; frame.style.position="fixed"; frame.style.top="0%"; frame.style.right="0%"; frame.style.height="800px"; frame.style.width="1400px"; frame.style.zIndex="100000"; document.body.appendChild(frame); var btn= document.createElement("button"); btn.style.position="fixed"; btn.style.top="0%"; btn.style.right="0%"; btn.zIndex="100000"; innerHTML="HIDE"; document.body.appendChild(btn);
+  var url = prompt('Cloak URL');var frame = document.createElement('iframe'); if(url.includes('https://') || url.includes("http://")) {frame.src = url;}else{frame.src = "https://" + url;} frame.style.position="fixed"; frame.style.top="0%"; frame.style.right="0%"; frame.style.height="800px"; frame.style.width="1400px"; frame.style.zIndex="100000"; document.body.appendChild(frame); var btn= document.createElement("button"); btn.style.position="fixed"; btn.style.top="0%"; btn.style.right="0%"; btn.zIndex="100000"; innerHTML="HIDE"; document.body.appendChild(btn);
   new PaymentRequest(
     [
       {
